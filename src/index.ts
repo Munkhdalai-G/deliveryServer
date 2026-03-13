44bc3a87c160ccda94618c25d3134fd24b2e0288
Munkhdalai-G <Bylzuuhai7@gmail.com>
2026-03-13 15:21:36 +0800

    g

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client";
 import express, { Request, Response } from "express";
 
 const app = express();
@@ -5,8 +6,40 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Hello, World!" });
+app.get("/users", async (req: Request, res: Response) => {
+  const users = await prisma.User.findMany();
+
+  res.json({ users });
+});
+
+app.get("/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      id: Number(id),
+    },
+  });
+
+  res.json({ user });
+});
+
+app.post("/users", async (req: Request, res: Response) => {
+  const { email, password, age } = req.body;
+
+  try {
+    const user = await Prisma.user.create({
+      data: {
+        email,
+        password,
+        age,
+      },
+    });
+
+    res.json({ user });
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 app.listen(PORT, () => {
